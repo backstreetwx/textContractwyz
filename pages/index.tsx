cff85d811ff5e5bfa95c84df525637af464198d6
黄雯珠 <huangwenzhu@huangwenzhudeiMac.local>
2024-03-27 18:10:13 +0900

    commit change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,100 +1,77 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
+
 import { NextPage } from "next";
+import { CONTRACT_ADDRESS } from "../constants/address";
+import { error } from "console";
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  const {
+    contract
+  } = useContract(CONTRACT_ADDRESS);
+
+  const {
+    data: counter,
+    isLoading: isCounterLoading,
+  }= useContractRead(contract, "getCounter")
+
+  const {
+    data: owner
+  } = useContractRead(contract, "owner")
+
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
-
-          <div className={styles.connect}>
-            <ConnectWallet />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
+        <ConnectWallet />
+        <h1>Counter Dapp</h1>
+        <div className={styles.counterContainer}>
+          {isCounterLoading ? (
+            <p 
+            style={{
+              fontSize: "5rem",
+              fontWeight: "bold",
+            }}
+            >0</p>
+          ) : (
+            <p
+            style={{
+              fontSize: "5rem",
+              fontWeight: "bold",
+            }}
+            >{counter}</p>
+          )}
+          {address && (
+            <div className={styles.buttonContainer}>
+              <Web3Button
+              contractAddress={CONTRACT_ADDRESS}
+              action={(contract) => contract.call(
+                "decrement"
+              )}
+              onError={(error) => alert(error)}
+              >-</Web3Button>
+              <Web3Button
+              contractAddress={CONTRACT_ADDRESS}
+              action={(contract) => contract.call(
+                "increment"
+              )}
+              onSuccess={() => alert("Counter incremented!")}
+              >+</Web3Button>
             </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
+          )}
+          {address == owner ? (
+            <Web3Button
+              contractAddress={CONTRACT_ADDRESS}
+              action={(contract) => contract.call(
+                "reset"
+              )}
+              >Reset</Web3Button>
+          ) : address ? (
+            <p>You are not the owner of this contract.</p>
+          ) : (
+            <p>Connect your wallet to interact with the counter!</p>
+          )}
         </div>
       </div>
     </main>
